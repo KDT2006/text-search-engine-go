ce4a51575510d94de3c5d2458ea65e05884d677c
KDT <developer.thiru@gmail.com>
2024-06-24 18:46:58 +0530

    Main file code

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/KDT2006/text-search-engine-go/utils"
 )
 
 func main() {
@@ -12,4 +16,23 @@ func main() {
 	flag.StringVar(&query, "q", "Small wild cat", "search query")
 	flag.Parse()
 	fmt.Println("Full text search is in progress...")
+	start := time.Now()
+	docs, err := utils.LoadDocuments(dumpPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Loaded %d documents in %v", len(docs), time.Since(start))
+
+	start = time.Now()
+	idx := make(utils.Index)
+	idx.Add(docs)
+	log.Printf("Indexed %d documents in %v", len(docs), time.Since(start))
+
+	start = time.Now()
+	matchedIDs := idx.Search(query)
+	log.Printf("Search found %d documents in %v", len(matchedIDs), time.Since(start))
+	for _, id := range matchedIDs {
+		doc := docs[id]
+		log.Printf("%d\t%s\n", id, doc.Text)
+	}
 }
